a20c1d0f133a38fdd255fdadb5729f1012a22975
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2020-01-11 01:59:28 +0330

    done with data type and run type script

--- dataType.js
+++ dataType.js
@@ -1,5 +1,6 @@
+"use strict";
 function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
+    const result = n1 + n2;
     if (showResult) {
         console.log(phrase + result);
     }
@@ -7,36 +8,37 @@ function add(n1, n2, showResult, phrase) {
         return result;
     }
 }
-var number1 = 5; // 5.0
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
+const number1 = 5; // 5.0
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
 /* -------------------------------------------------------------------------- */
 /*                                   object                                   */
 /* -------------------------------------------------------------------------- */
-var person = {
+const person = {
     name: 'hamid',
     age: 30
 };
-var person1 = {
+const person1 = {
     name: 'hamid',
     age: 30
 };
-var person2 = {
+const person2 = {
     name: 'hamid',
     age: 30
 };
 /* -------------------------------------------------------------------------- */
 /*                                    Array                                   */
 /* -------------------------------------------------------------------------- */
-var array1;
-var array2;
-var array3;
+let array1;
+let array2;
+let array3;
 /* -------------------------------------------------------------------------- */
 /*                                    tuple                                   */
 /* -------------------------------------------------------------------------- */
-var tuple1;
-var tuple2;
+let tuple1;
+let tuple2;
 tuple1 = [1, "hamid"];
 tuple2 = [1, "hamid1", 2, 3, 4];
 /* -------------------------------------------------------------------------- */
@@ -49,4 +51,68 @@ var enum1;
     enum1[enum1["AUTHOR"] = 3] = "AUTHOR";
 })(enum1 || (enum1 = {}));
 ;
-add(number1, number2, printResult, resultPhrase);
+/* -------------------------------------------------------------------------- */
+/*                                     any                                    */
+/* -------------------------------------------------------------------------- */
+let any1;
+/* -------------------------------------------------------------------------- */
+/*                                    UNION                                   */
+/* -------------------------------------------------------------------------- */
+function unionExample(input1, input2) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinedAges = unionExample(30, 26);
+const combinedNames = unionExample('Max', 'Anna');
+/* -------------------------------------------------------------------------- */
+/*                                literal Type                                */
+/* -------------------------------------------------------------------------- */
+let literal1;
+/* -------------------------------------------------------------------------- */
+/*                       function return types and void                       */
+/* -------------------------------------------------------------------------- */
+function returnExample(n1, n2) {
+    return n1 + n2;
+}
+function voidTypeReturnExample(n1) {
+    console.log('result' + n1);
+}
+/* -------------------------------------------------------------------------- */
+/*                                function type                               */
+/* -------------------------------------------------------------------------- */
+let functionVariable;
+function functionTypeExample(name) {
+    console.log(name);
+}
+functionVariable = functionTypeExample;
+let functionVariable1; // just function who have two input parameters and one callback function and string return type
+/* -------------------------------------------------------------------------- */
+/*                                   unknown                                  */
+/* -------------------------------------------------------------------------- */
+let unknownEG;
+let anyEG;
+let unknownConflictExample;
+unknownEG = 5;
+unknownEG = "feiife";
+anyEG = "feiife"; // unknownConflictExample = unknownEG;// this is defernt between any and unknown type
+/* -------------------------------------------------------------------------- */
+/*                                 never type                                 */
+/* -------------------------------------------------------------------------- */
+// there are two kind of function that never return
+function generateNeverReturnEg1() {
+    throw { message: 'msg' };
+}
+function generateNeverReturnEg2() {
+    while (true) {
+    }
+}
+let promise = new Promise(function (resolve, reject) {
+    // after 1 second signal that the job is finished with an error
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
--- dataType.ts
+++ dataType.ts
@@ -12,6 +12,7 @@ const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
 
+add(number1, number2, printResult, resultPhrase);
 
 /* -------------------------------------------------------------------------- */
 /*                                   object                                   */
@@ -47,12 +48,108 @@ let array3: any[];
 /* -------------------------------------------------------------------------- */
 let tuple1: [number, string];
 let tuple2: [number, string, number, number, number];
-tuple1 = [1,"hamid"]
-tuple2 = [1, "hamid1",2,3,4]
+tuple1 = [1, "hamid"]
+tuple2 = [1, "hamid1", 2, 3, 4]
 
 /* -------------------------------------------------------------------------- */
 /*                                    enum                                    */
 /* -------------------------------------------------------------------------- */
-enum enum1 { ADMIN = 1, READ_ONLY = 2, AUTHOR = 3 }; 
+enum enum1 { ADMIN = 1, READ_ONLY = 2, AUTHOR = 3 };
 
-add(number1, number2, printResult, resultPhrase);
+
+/* -------------------------------------------------------------------------- */
+/*                                     any                                    */
+/* -------------------------------------------------------------------------- */
+let any1: any
+
+/* -------------------------------------------------------------------------- */
+/*                                    UNION                                   */
+/* -------------------------------------------------------------------------- */
+function unionExample(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+const combinedAges = unionExample(30, 26);
+const combinedNames = unionExample('Max', 'Anna');
+
+
+/* -------------------------------------------------------------------------- */
+/*                                literal Type                                */
+/* -------------------------------------------------------------------------- */
+let literal1: 'as-number' | 'as-text';
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   Aliases                                  */
+/* -------------------------------------------------------------------------- */
+
+type aliases1 = number | string;
+
+
+/* -------------------------------------------------------------------------- */
+/*                       function return types and void                       */
+/* -------------------------------------------------------------------------- */
+function returnExample(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+function voidTypeReturnExample(n1: number): void {
+    console.log('result' + n1);
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                                function type                               */
+/* -------------------------------------------------------------------------- */
+
+let functionVariable: Function;
+
+function functionTypeExample(name: String) {
+    console.log(name);
+}
+
+functionVariable = functionTypeExample;
+
+
+
+
+
+let functionVariable1: (a: number, b: number, cb: (num: number) => void) => String;   // just function who have two input parameters and one callback function and string return type
+
+/* -------------------------------------------------------------------------- */
+/*                                   unknown                                  */
+/* -------------------------------------------------------------------------- */
+
+let unknownEG: unknown;
+let anyEG: any;
+let unknownConflictExample: string;
+
+unknownEG = 5;
+unknownEG = "feiife";
+anyEG = "feiife";// unknownConflictExample = unknownEG;// this is defernt between any and unknown type
+
+/* -------------------------------------------------------------------------- */
+/*                                 never type                                 */
+/* -------------------------------------------------------------------------- */
+// there are two kind of function that never return
+function generateNeverReturnEg1(): never {
+    throw { message: 'msg' };
+}
+function generateNeverReturnEg2(): never {
+    while (true) {
+
+    }
+}
+
+
+let promise = new Promise(function (resolve, reject) {
+    // after 1 second signal that the job is finished with an error
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
